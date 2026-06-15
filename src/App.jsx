cf66c0304af6b62664c38a0ece7d77e660697c8b
ishjyot-gh <ishjyot@gmail.com>
2026-06-15 08:04:34 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -23,36 +23,24 @@ function App() {
       <LayersControl position="topright">
 
         <LayersControl.Overlay checked name="India">
-          <GeoJsonLayer
-            url="/data/india.geojson"
-          />
+        <GeoJsonLayer url={`${import.meta.env.BASE_URL}data/india.geojson`} />
         </LayersControl.Overlay>
-
+        
         <LayersControl.Overlay checked name="States">
-          <GeoJsonLayer
-            url="/data/states.geojson"
-          />
+          <GeoJsonLayer url={`${import.meta.env.BASE_URL}data/states.geojson`} />
         </LayersControl.Overlay>
-
-        <LayersControl.Overlay
-          name="Districts"
-        >
-          <GeoJsonLayer
-            url="/data/districts.geojson"
-          />
+        
+        <LayersControl.Overlay name="Districts">
+          <GeoJsonLayer url={`${import.meta.env.BASE_URL}data/districts.geojson`} />
         </LayersControl.Overlay>
-
-        <LayersControl.Overlay
-          name="Taluks"
-        >
-          <GeoJsonLayer
-            url="/data/taluks.geojson"
-          />
+        
+        <LayersControl.Overlay name="Taluks">
+          <GeoJsonLayer url={`${import.meta.env.BASE_URL}data/taluks.geojson`} />
         </LayersControl.Overlay>
 
       </LayersControl>
 
-
+      
     </MapContainer>
   );
 }
